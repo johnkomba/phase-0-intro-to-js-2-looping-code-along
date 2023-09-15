a2ac97df1d7f7a0b2e895484ac5a125164f782fd
Jkomba <johnkombah88@gmail.com>
2023-09-15 15:26:38 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  countDown(10);
+  function writeCards(names, eventName) {
+    let messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let message = `Dear ${names[i]}, thank you for coming to the ${eventName} event!`;
+      messages.push(message);
+    }
+  
+    return messages;
+  }
+  
